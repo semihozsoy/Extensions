bac7b2b250dce41272fa6b84847a8f90af047c57
Semih Özsoy <semih__ozsoy@hotmail.com>
2023-10-01 12:36:06 +0300

    uiview extension implemented

---
+++ Sources/Extensions/UIView+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Semih Ozsoy on 28.09.2023.
+//
+
+import UIKit
+
+public extension UIView {
+    
+    class var identifier: String {
+        String(describing: self)
+    }
+}
